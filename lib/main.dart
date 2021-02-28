a874efa4a78162bb11f77946751e27c49be2efc9
Tore <tore.buscher@integra-ev.de>
2021-02-28 20:16:20 +0100

    "2Button"

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,32 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wilkommen bei UniMa Tutor',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Wilkommen in der App'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      )
+        title: 'Wilkommen bei UniMa Tutor',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Wilkommen in der App'),
+            ),
+            body: Center(
+              child: Column(children: [
+                TextButton(
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  child: Text("TEXT BUTTON1"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Respond to button press
+                  },
+                  child: Text("TEXT BUTTON2"),
+                )
+              ]),
+            )
+        )
     );
   }
-
 }
